82b62e552ab83fcb6d4eff61352d2139252c76e8
ctyode <irribarrenya@gmail.com>
2016-07-09 00:14:33 +0300

    dp

--- app/src/main/java/org/flamie/moon/MainActivity.java
+++ app/src/main/java/org/flamie/moon/MainActivity.java
@@ -2,11 +2,15 @@ package org.flamie.moon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.flamie.moon.util.Dimen;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Dimen.init(this);
         setContentView(new MoonView(this));
     }
 
--- app/src/main/java/org/flamie/moon/MoonView.java
+++ app/src/main/java/org/flamie/moon/MoonView.java
@@ -1,5 +1,7 @@
 package org.flamie.moon;
 
+import static org.flamie.moon.util.Dimen.dp;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,22 +12,44 @@ import android.view.View;
 
 public class MoonView extends View {
 
+//    private int canvasWidth = this.getWidth() / 2;
+//    private int canvasHeight = this.getHeight() / 2;
     private float radius = 200f;
-    private float x = 500f;
-    private float y = 500f;
+    private float xMoon = 500f;
+    private float yMoon = 500f;
+    private float x;
+    private float y;
+    private float rectWidth;
+    private float rectHeight;
 
-    private Paint paint;
+    private Paint moonPaint;
+    private Paint skyPaint;
+    private Paint cloudPaint;
     private RectF farSideOfTheMoon;
     private RectF nearSideOfTheMoon;
+    private RectF cloudRect;
+    private RectF cloud;
 
     public MoonView(Context context) {
         super(context);
-        paint = new Paint();
-        paint.setColor(Color.YELLOW);
-        paint.setStrokeWidth(5f);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setAntiAlias(true);
+        moonPaint = new Paint();
+        moonPaint.setColor(Color.YELLOW);
+        moonPaint.setStrokeWidth(5f);
+        moonPaint.setStyle(Paint.Style.STROKE);
+        moonPaint.setAntiAlias(true);
 
+        skyPaint = new Paint();
+        skyPaint.setColor(Color.argb(255, 48, 63, 159));
+        skyPaint.setStyle(Paint.Style.FILL);
+        skyPaint.setAntiAlias(true);
+
+        cloudPaint = new Paint();
+        cloudPaint.setColor(Color.argb(255, 48, 63, 159));
+        skyPaint.setStyle(Paint.Style.FILL);
+        cloudPaint.setAntiAlias(true);
+
+        cloud = new RectF();
+        cloudRect = new RectF();
         farSideOfTheMoon = new RectF();
         nearSideOfTheMoon = new RectF();
     }
@@ -33,24 +57,25 @@ public class MoonView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
-        farSideOfTheMoon.set(x - radius, y - radius, x + radius, y + radius);
-        nearSideOfTheMoon.set((x - 100f) - radius, (y - 100f) - radius, (x - 100f) + radius, (y - 100f) + radius);
+        farSideOfTheMoon.set(xMoon - radius, yMoon - radius, xMoon + radius, yMoon + radius);
+        nearSideOfTheMoon.set((xMoon - 100f) - radius, (yMoon - 100f) - radius, (xMoon - 100f) + radius, (yMoon - 100f) + radius);
 
-        canvas.drawArc(farSideOfTheMoon, 295f, 220f, false, paint);
-        canvas.drawArc(nearSideOfTheMoon, 335f, 140f, false, paint);
+        canvas.drawRect(dp(1), dp(20), dp(10), dp(40), cloudPaint);
+        canvas.drawArc(farSideOfTheMoon, 295f, 220f, false, moonPaint);
+        canvas.drawArc(nearSideOfTheMoon, 335f, 140f, false, moonPaint);
     }
 
     @Override
     public boolean onTouchEvent (MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                x = event.getX();
-                y = event.getY();
+                xMoon = event.getX();
+                yMoon = event.getY();
                 invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
-                x = event.getX();
-                y = event.getY();
+                xMoon = event.getX();
+                yMoon = event.getY();
                 invalidate();
                 break;
         }
